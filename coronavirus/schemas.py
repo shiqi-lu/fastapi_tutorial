c38578cf83d032c4752345369690559c7abb613d
Traum Lou <traumlou@gmail.com>
2021-07-14 15:43:53 +0800

    7-3 使用Pydantic建立与模型类对应的数据格式类

--- coronavirus/schemas.py
+++ coronavirus/schemas.py
@@ -1,1 +1,39 @@
- # 与数据库表一一对应的继承 pydantic basemodel 的模型类
+# 与数据库表一一对应的继承 pydantic basemodel 的模型类
+
+from datetime import date as date_
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class CreateData(BaseModel):
+    date: date_
+    confirmed: int = 0
+    deaths: int = 0
+    recovered: int = 0
+
+
+class CreateCity(BaseModel):
+    province: str
+    country: str
+    country_code: str
+    country_population: int
+
+
+class ReadData(CreateData):
+    id: int
+    city_id: int
+    updated_at: datetime
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class ReadCity(CreateCity):
+    id: int
+    updated_at: datetime
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
