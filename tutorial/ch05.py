de9825f92f1bf42dad948fc50c768614add1471a
Traum Lou <traumlou@gmail.com>
2021-07-19 00:51:05 +0800

    5-3 如何将类作为依赖项?

--- tutorial/ch05.py
+++ tutorial/ch05.py
@@ -21,3 +21,26 @@ async def dependency01(commons: dict = Depends(common_parameters)):
 def dependency02(commons: dict = Depends(common_parameters)):
     # 可以在async def中调用def依赖，也可以在def中导入async def依赖
     return commons
+
+
+"""Classes as Dependencies 类作为依赖项"""
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"},
+                 {"item_name": "Baz"}]
+
+
+class CommonQueryParams:
+    def __init__(self, q: Optional[str] = None, page: int = 1, limit: int = 100):
+        self.q = q
+        self.page = page
+        self.limit = limit
+
+
+@app05.get("/classes_as_dependencies")
+async def classes_as_dependencies(commons=Depends(CommonQueryParams)):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.page: commons.page + commons.limit]
+    response.update({"items": items})
+    return response
